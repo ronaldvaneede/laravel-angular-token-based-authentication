77042e75b3474619606bf90ccc75cc3d8794b778
vagrant <vagrant@homestead>
2015-10-12 19:19:27 +0000

    Working app

---
+++ public/scripts/authController.js
@@ -0,0 +1,21 @@
+(function() {
+	'use strict';
+
+	angular
+		.module('authApp')
+		.controller('AuthController', AuthController);
+
+	function AuthController($auth, $state) {
+		var vm = this;
+		vm.login = function() {
+			var credentials = {
+				email: vm.email,
+				password: vm.password
+			}
+
+			$auth.login(credentials).then(function(data) {
+				$state.go('users', {});
+			})
+		}
+	}
+})();
---
+++ readme.md
@@ -0,0 +1,3 @@
+## Token-Based Authentication for AngularJS and Laravel Apps
+
+Result of following the [Token-Based Authentication for AngularJS and Laravel Apps](https://scotch.io/tutorials/token-based-authentication-for-angularjs-and-laravel-apps) tutorial on [scotch.io](https://scotch.io)
